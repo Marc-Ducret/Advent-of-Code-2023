d99851d9679ff9b3c64d806e2f06387498c7d7cd
Marc Ducret <marc.t.ducret@gmail.com>
2023-12-04 16:15:56 +0100

    Day 4.

---
+++ Problems/Day04A.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2023;
+
+public partial class Day04A : Problem<Day04A.Input, int> {
+    public readonly record struct Input(Input.Card[] Cards) {
+        public readonly record struct Card(int Id, int[] Winning, int[] Have);
+    }
+
+    protected override Input PreProcess(TextReader input) {
+        List<Input.Card> cards = new();
+        while (input.ReadLine() is { } line) {
+            Match match = RowRegex().Match(line);
+            if (!match.Success) continue;
+            cards.Add(
+                new Input.Card(
+                    int.Parse(match.Groups["id"].Value),
+                    match.Groups["winning"].Value.Split(" ")
+                         .Where(s => s.Length > 0)
+                         .Select(int.Parse)
+                         .ToArray(),
+                    match.Groups["have"].Value.Split(" ")
+                         .Where(s => s.Length > 0)
+                         .Select(int.Parse)
+                         .ToArray()
+                )
+            );
+        }
+
+        return new Input(cards.ToArray());
+    }
+
+    protected override int Solve(Input input) =>
+        input.Cards.Sum(Score);
+
+    private int Score(int matches) =>
+        matches > 0
+            ? 1 << (matches - 1)
+            : 0;
+
+    private int Score(Input.Card card) =>
+        Score(card.Have.Intersect(card.Winning).Count());
+
+    public static void Run() {
+        new Day04A().Solve();
+    }
+
+    [GeneratedRegex(@"Card\s+(?<id>\d+):\s*(?<winning>[\d\s]*)\s*\|\s*(?<have>[\d\s]*)\s*")]
+    private static partial Regex RowRegex();
+}
---
+++ Problems/Day04B.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2023;
+
+public partial class Day04B : Problem<Day04B.Input, int> {
+    public readonly record struct Input(Input.Card[] Cards) {
+        public readonly record struct Card(int Id, int[] Winning, int[] Have);
+    }
+
+    protected override Input PreProcess(TextReader input) {
+        List<Input.Card> cards = new();
+        while (input.ReadLine() is { } line) {
+            Match match = RowRegex().Match(line);
+            if (!match.Success) continue;
+            cards.Add(
+                new Input.Card(
+                    int.Parse(match.Groups["id"].Value),
+                    match.Groups["winning"].Value.Split(" ")
+                         .Where(s => s.Length > 0)
+                         .Select(int.Parse)
+                         .ToArray(),
+                    match.Groups["have"].Value.Split(" ")
+                         .Where(s => s.Length > 0)
+                         .Select(int.Parse)
+                         .ToArray()
+                )
+            );
+        }
+
+        return new Input(cards.ToArray());
+    }
+
+    protected override int Solve(Input input) {
+        int[] counts = new int[input.Cards.Length];
+        for (int i = 0; i < input.Cards.Length; i++) {
+            counts[i] = 1;
+        }
+        
+        for (int i = 0; i < input.Cards.Length; i++) {
+            Input.Card card = input.Cards[i];
+            for (int j = 1; j <= Matches(card); j++) {
+                if (i + j < input.Cards.Length)
+                    counts[i + j] += counts[i];
+            }
+        }
+
+        return counts.Sum();
+    }
+
+    private int Matches(Input.Card card) =>
+        card.Have.Intersect(card.Winning).Count();
+
+    public static void Run() {
+        new Day04B().Solve();
+    }
+
+    [GeneratedRegex(@"Card\s+(?<id>\d+):\s*(?<winning>[\d\s]*)\s*\|\s*(?<have>[\d\s]*)\s*")]
+    private static partial Regex RowRegex();
+}
