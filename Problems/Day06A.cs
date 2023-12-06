052a473237c76dc51e891e837d51cae5e3163ed0
Marc Ducret <marc.t.ducret@gmail.com>
2023-12-07 00:58:48 +0100

    Day 6.

---
+++ Problems/Day06A.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2023;
+
+public partial class Day06A : Problem<Day06A.Input, int> {
+    public readonly record struct Input(Input.Race[] Races) {
+        public readonly record struct Race(int Time, int DistanceRecord);
+    }
+
+    protected override Input PreProcess(TextReader input) {
+        int[] times           = Numbers(input.ReadLine()!);
+        int[] distanceRecords = Numbers(input.ReadLine()!);
+
+        return new Input(times.Zip(distanceRecords).Select(pair => new Input.Race(pair.First, pair.Second)).ToArray());
+
+        static int[] Numbers(string s) => NumberRegex().Matches(s).Select(m => int.Parse(m.Value)).ToArray();
+    }
+
+    public int Distance(int pressTime, int totalTime) => pressTime * (totalTime - pressTime);
+
+    public int WaysToWins(Input.Race race) =>
+        Enumerable.Range(0, race.Time + 1).Count(pressTime => Distance(pressTime, race.Time) > race.DistanceRecord);
+
+    protected override int Solve(Input input) =>
+        input.Races
+             .Select(WaysToWins)
+             .Aggregate((a, b) => a * b);
+
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex NumberRegex();
+
+    public static void Run() {
+        new Day06A().Solve();
+    }
+}
---
+++ Problems/Day06B.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2023;
+
+public partial class Day06B : Problem<Day06B.Input, int> {
+    public readonly record struct Input(Input.Race race) {
+        public readonly record struct Race(long Time, long DistanceRecord);
+    }
+
+    protected override Input PreProcess(TextReader input) {
+        long[] times           = Numbers(input.ReadLine()!.Replace(" ", ""));
+        long[] distanceRecords = Numbers(input.ReadLine()!.Replace(" ", ""));
+
+        return new Input(times.Zip(distanceRecords).Select(pair => new Input.Race(pair.First, pair.Second)).First());
+
+        static long[] Numbers(string s) => NumberRegex().Matches(s).Select(m => long.Parse(m.Value)).ToArray();
+    }
+
+    public long Distance(long pressTime, long totalTime) => pressTime * (totalTime - pressTime);
+
+    public int WaysToWins(Input.Race race) {
+        double extent = Math.Sqrt(Math.Pow(race.Time, 2) - 4   * race.DistanceRecord);
+        int    start  = (int)Math.Ceiling((race.Time - extent) / 2);
+        int    end    = (int)Math.Floor((race.Time   + extent) / 2);
+
+        return end - start + 1;
+    }
+
+    protected override int Solve(Input input) =>
+        WaysToWins(input.race);
+
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex NumberRegex();
+
+    public static void Run() {
+        new Day06B().Solve();
+    }
+}
