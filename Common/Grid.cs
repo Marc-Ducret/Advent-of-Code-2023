6b628c39eddf3eafeab83aaa7e95f5b0e69ff059
Marc Ducret <marc.t.ducret@gmail.com>
2023-12-16 18:45:56 +0100

    Day 16.

---
+++ Common/Grid.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+
+namespace Advent_of_Code_2023;
+
+public readonly struct Grid<T> : IEquatable<Grid<T>>
+    where T : struct, IEquatable<T>, IGridElement {
+    public  Int2 Size   { get; }
+    private T[,] Values { get; }
+
+    public Grid(Int2 size) {
+        Size   = size;
+        Values = new T[size.X, size.Y];
+    }
+
+    public bool IsWithin(in Int2 position) =>
+        ((position >= 0) & (position < Size)).All;
+
+    public T this[in Int2 position] {
+        get =>
+            IsWithin(position)
+                ? Values[position.X, position.Y]
+                : default;
+        set {
+            if (IsWithin(position))
+                Values[position.X, position.Y] = value;
+        }
+    }
+
+    public IEnumerable<Int2> Positions() {
+        Int2 size = Size;
+        return Enumerable.Range(0, size.Y)
+                         .SelectMany(
+                              y => Enumerable.Range(0, size.X)
+                                             .Select(x => new Int2(x, y)));
+    }
+
+    public string DebugString {
+        get {
+            StringBuilder builder = new();
+            for (int y = Size.Y - 1; y >= 0; y--) {
+                for (int x = 0; x < Size.X; x++) {
+                    builder.Append(this[new Int2(x, y)].DebugChar());
+                }
+
+                builder.Append('\n');
+            }
+
+            return builder.ToString();
+        }
+    }
+
+    public Grid<T> Copy() {
+        Grid<T> grid = new(Size);
+        foreach (Int2 position in Positions()) {
+            grid[position] = this[position];
+        }
+
+        return grid;
+    }
+
+    public override int GetHashCode() {
+        HashCode hash = new();
+        foreach (Int2 position in Positions()) {
+            hash.Add(this[position]);
+        }
+
+        return hash.ToHashCode();
+    }
+
+    public bool Equals(Grid<T> other) {
+        foreach (Int2 position in Positions()) {
+            if (!this[position].Equals(other[position]))
+                return false;
+        }
+
+        return true;
+    }
+
+    public override bool Equals(object?      obj)                 => obj is Grid<T> other && Equals(other);
+    public static   bool operator ==(Grid<T> left, Grid<T> right) => left.Equals(right);
+    public static   bool operator !=(Grid<T> left, Grid<T> right) => !(left == right);
+
+    public static Grid<T> Parse(string input) {
+        string[] lines = input.Split('\n');
+        Grid<T>  grid  = new(new Int2(lines.Min(l => l.Length), lines.Length));
+
+        foreach (Int2 position in grid.Positions()) {
+            T value = default;
+            value.Parse(lines[grid.Size.Y - position.Y - 1][position.X]);
+            grid[position] = value;
+        }
+
+        return grid;
+    }
+}
---
+++ Problems/Day16A.cs
@@ -0,0 +1,154 @@
+﻿namespace Advent_of_Code_2023;
+
+public class Day16A : Problem<Day16A.Input, int> {
+    public readonly record struct Input(Grid<TileElement> Grid);
+
+    public enum Tile {
+        EMPTY,
+        MIRROR_POSITIVE,
+        MIRROR_NEGATIVE,
+        SPLITTER_HORIZONTAL,
+        SPLITTER_VERTICAL
+    }
+
+    [Flags]
+    public enum Direction : byte {
+        EMPTY = 0,
+        EAST  = 1 << 0,
+        NORTH = 1 << 1,
+        WEST  = 1 << 2,
+        SOUTH = 1 << 3
+    }
+
+    private static readonly Dictionary<Direction, Int2> directionOffset = new() {
+        [Direction.EAST]  = new Int2(+1, +0),
+        [Direction.NORTH] = new Int2(+0, +1),
+        [Direction.WEST]  = new Int2(-1, +0),
+        [Direction.SOUTH] = new Int2(+0, -1)
+    };
+
+    private static readonly Dictionary<Int2, Direction> offsetToDirection =
+        directionOffset.ToDictionary(entry => entry.Value, entry => entry.Key);
+
+    private static readonly Dictionary<Direction, Direction> directionOpposite = new() {
+        [Direction.EAST]  = Direction.WEST,
+        [Direction.NORTH] = Direction.SOUTH,
+        [Direction.WEST]  = Direction.EAST,
+        [Direction.SOUTH] = Direction.NORTH
+    };
+
+    public record struct TileElement(Tile Value) : IGridElement {
+        private static readonly (char @char, Tile tile)[] charsPairings = [
+            ('.', Tile.EMPTY),
+            ('/', Tile.MIRROR_POSITIVE),
+            ('\\', Tile.MIRROR_NEGATIVE),
+            ('-', Tile.SPLITTER_HORIZONTAL),
+            ('|', Tile.SPLITTER_VERTICAL)
+        ];
+
+        private static readonly Dictionary<char, Tile> parse =
+            charsPairings.ToDictionary();
+
+        private static readonly Dictionary<Tile, char> debug =
+            charsPairings.ToDictionary(pair => pair.tile, pair => pair.@char);
+
+        public char DebugChar()   => debug.GetValueOrDefault(Value, '?');
+        public void Parse(char c) => Value = parse[c];
+
+        public static implicit operator Tile(TileElement element) => element.Value;
+        public static implicit operator TileElement(Tile value)   => new(value);
+    }
+
+    public record struct DirectionElement(Direction Value) : IGridElement {
+        private static readonly (char @char, Direction tile)[] charsPairings = [
+            ('.', Direction.EMPTY),
+            ('→', Direction.EAST),
+            ('↑', Direction.NORTH),
+            ('←', Direction.WEST),
+            ('↓', Direction.SOUTH)
+        ];
+
+        private static readonly Dictionary<char, Direction> parse =
+            charsPairings.ToDictionary();
+
+        private static readonly Dictionary<Direction, char> debug =
+            charsPairings.ToDictionary(pair => pair.tile, pair => pair.@char);
+
+        public char DebugChar()   => debug.GetValueOrDefault(Value, '?');
+        public void Parse(char c) => Value = parse[c];
+
+        public static implicit operator Direction(DirectionElement element) => element.Value;
+        public static implicit operator DirectionElement(Direction value)   => new(value);
+    }
+
+    protected override Input PreProcess(string input) =>
+        new(Grid<TileElement>.Parse(input));
+
+    private Grid<DirectionElement> SpreadLight(Grid<TileElement> grid) {
+        Grid<DirectionElement> light = new(grid.Size);
+
+        Stack<(Int2 position, Direction direction)> spreadStack = new();
+        spreadStack.Push((new Int2(0, grid.Size.Y - 1), Direction.EAST));
+        while (spreadStack.TryPop(out var spread)) {
+            if (!grid.IsWithin(spread.position))
+                continue;
+            if ((light[spread.position] & spread.direction) != Direction.EMPTY)
+                continue;
+
+            light[spread.position] |= spread.direction;
+            switch (grid[spread.position].Value) {
+                case Tile.EMPTY: {
+                    Spread(spread.direction);
+                    break;
+                }
+                case Tile.MIRROR_POSITIVE: {
+                    Int2 offset = directionOffset[spread.direction];
+                    Spread(offsetToDirection[new Int2(offset.Y, offset.X)]);
+                    break;
+                }
+                case Tile.MIRROR_NEGATIVE: {
+                    Int2 offset = directionOffset[spread.direction];
+                    Spread(offsetToDirection[-new Int2(offset.Y, offset.X)]);
+                    break;
+                }
+                case Tile.SPLITTER_HORIZONTAL: {
+                    if ((spread.direction & (Direction.EAST | Direction.WEST)) != Direction.EMPTY)
+                        Spread(spread.direction);
+                    else {
+                        Spread(Direction.EAST);
+                        Spread(Direction.WEST);
+                    }
+
+                    break;
+                }
+                case Tile.SPLITTER_VERTICAL: {
+                    if ((spread.direction & (Direction.NORTH | Direction.SOUTH)) != Direction.EMPTY)
+                        Spread(spread.direction);
+                    else {
+                        Spread(Direction.NORTH);
+                        Spread(Direction.SOUTH);
+                    }
+
+                    break;
+                }
+            }
+
+            continue;
+
+            void Spread(Direction direction) =>
+                spreadStack.Push((spread.position + directionOffset[direction], direction));
+        }
+
+        return light;
+    }
+
+    protected override int Solve(Input input) {
+        Grid<DirectionElement> light = SpreadLight(input.Grid);
+
+        return light.Positions().Count(p => light[p] != Direction.EMPTY);
+    }
+
+    public static void Run() {
+        new Day16A().Solve();
+    }
+}
